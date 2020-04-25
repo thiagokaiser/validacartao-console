11d76c577c24f4c5e90ab58ede80b263b8a93d5a
unknown <thiago.kaiser@hotmail.com>
2020-04-25 14:47:03 -0300

    ajuste tests

--- Testes/CartaoTest1.cs
+++ Testes/CartaoTest1.cs
@@ -2,6 +2,7 @@ using ValidaCartaoTest;
 using ValidaCartaoTest.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using ValidaCartaoTest.Bandeiras;
 
 namespace Testes
 {
@@ -11,41 +12,49 @@ namespace Testes
         [TestMethod]
         public void TestInverteNumero()
         {
-            var service = new CartaoService();
-            var numeroInvertido = service.InverteNumero("12345");
+            var cartao = new Amex();
+
+            var numeroInvertido = cartao.InverteNumero("12345");
             var teste = new List<int>() { 5, 4, 3, 2, 1 };
 
             CollectionAssert.AreEqual(numeroInvertido, teste);            
         }
+
         [TestMethod]
         public void TestDobraNumeroes()
         {
-            var service = new CartaoService();
-            var numeroDobrado = service.DobraNumeros(new List<int>() { 1,2,3,4,5 });
+            var cartao = new Amex();
+            var numeroDobrado = cartao.DobraNumeros(new List<int>() { 1,2,3,4,5 });
 
             var teste = new List<int>() { 1,4,3,8,5 };
 
             CollectionAssert.AreEqual(numeroDobrado, teste);
         }
+
         [TestMethod]
         public void TestValidaCartaoFalse()
         {
             var service = new CartaoService();
-            var cartao = new Cartao("12345");
 
-            service.Valida(cartao);
-
-            Assert.IsFalse(cartao.Valido);
+            try
+            {
+                service.ValidaCartao("12345");
+                Assert.Fail("no exception thrown");
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is CartaoInvalidoExcepion);
+            }
         }
+
         [TestMethod]
         public void TestValidaCartaoTrue()
         {
             var service = new CartaoService();
-            var cartao = new Cartao("4111111111111111");
 
-            service.Valida(cartao);
-
-            Assert.IsTrue(cartao.Valido);
+            var result = service.ValidaCartao("4111111111111111");
+            Assert.IsTrue(result is CartaoBase);
+            
         }
     }
 }
